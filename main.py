176ad6ff6ab3643256e3e9be191078c576a96c60
Roland Coburger <r.coburger@joke.de>
2024-10-16 07:01:13 +0200

    Joke Meldungen

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def index():
 def handle_message():
         message = request.form['message_input']
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.sendto(message.encode('cp1252'), ('192.168.4.11', 51522))
-        '''
+        #s.sendto(message.encode('cp1252'), ('192.168.4.11', 51522))
+
         for i in range(1,254):
-             s.sendto(message.encode('utf-8'), ('192.168.48.'+str(i), 51522))
+             s.sendto(message.encode('cp1252'), ('192.168.48.'+str(i), 51522))
 
         for i in range(2, 254):
-             s.sendto(message.encode('utf-8'), ('192.168.4.'+str(i), 51522))'''
+             s.sendto(message.encode('cp1252'), ('192.168.4.'+str(i), 51522))
 
         return render_template("index.html")
 
